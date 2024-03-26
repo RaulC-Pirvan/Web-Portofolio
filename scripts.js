3b9e6c6f19fc6e67481489f63610764ac0536428
RaulC-Pirvan <RaulC.Pirvan@outlook.com>
2024-03-26 12:25:18 +0200

    [26032024-1225] Fixed some stuff

--- scripts.js
+++ scripts.js
@@ -1,34 +1,41 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Function to handle smooth scrolling to sections
+    function scrollToSection(sectionId) {
+        const section = document.getElementById(sectionId);
+        section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+
+    // Smooth scrolling for navigation buttons
     document.getElementById('aboutBtn').addEventListener('click', function() {
-        const projectsSection = document.getElementById('about');
-        projectsSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        scrollToSection('about');
     });
-    
+
     document.getElementById('projectsBtn').addEventListener('click', function() {
-        const projectsSection = document.getElementById('projects');
-        projectsSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        scrollToSection('projects');
     });
-    
+
     document.getElementById('contactBtn').addEventListener('click', function() {
-        const projectsSection = document.getElementById('contact');
-        projectsSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        scrollToSection('contact');
     });
 
-    window.onscroll = function() {
-        scrollFunction();
-    };
+    // Hide loading screen when the document is fully loaded
+    document.getElementById('loading-screen').style.display = 'none';
 
-    function scrollFunction() {
+    // Show/hide scroll-to-top button based on scroll position
+    window.addEventListener('scroll', function() {
         var scrollBtn = document.getElementById("scrollTopBtn");
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        if (window.scrollY > 20) {
             scrollBtn.style.display = "block";
         } else {
             scrollBtn.style.display = "none";
         }
-    }
+    });
 
-    function scrollToTop() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-    }
+    // Smooth scroll to top when scroll-to-top button is clicked
+    document.getElementById("scrollTopBtn").addEventListener('click', function() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
 });
