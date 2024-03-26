e1f0e43a52ec7036d3bf4d106bad7e39907fa6b6
RaulC-Pirvan-TUIASI <Raul-Costin.Pirvan@student.tuiasi.ro>
2024-03-26 11:18:57 +0200

    [26032024-1118] Added "top" button

--- scripts.js
+++ scripts.js
@@ -13,5 +13,22 @@ document.addEventListener('DOMContentLoaded', function() {
         const projectsSection = document.getElementById('contact');
         projectsSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
     });
-});
 
+    window.onscroll = function() {
+        scrollFunction();
+    };
+
+    function scrollFunction() {
+        var scrollBtn = document.getElementById("scrollTopBtn");
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            scrollBtn.style.display = "block";
+        } else {
+            scrollBtn.style.display = "none";
+        }
+    }
+
+    function scrollToTop() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+});
